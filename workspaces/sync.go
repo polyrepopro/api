c1a55f671216a6403f3209109b04153034fdc9a7
Matthew Davis <matthew@matthewdavis.io>
2024-08-04 20:16:01 -0500

    bump

--- workspaces/sync.go
+++ workspaces/sync.go
@@ -1,11 +1,33 @@
 package workspaces
 
-import "github.com/polyrepopro/api/config"
+import (
+	"fmt"
+	"os"
+
+	"github.com/polyrepopro/api/config"
+	"github.com/polyrepopro/api/git"
+)
 
 type SyncArgs struct {
 }
 
 func Sync(workspace *config.Workspace, args SyncArgs) error {
 	for _, repo := range *workspace.Repositories {
+		// repoPath := repo.GetAbsolutePath()
+		if _, err := os.Stat(repo.Path); os.IsNotExist(err) {
+			// err := os.MkdirAll(filepath.Dir(repoPath), 0755)
+			// if err != nil {
+			// 	return fmt.Errorf("failed to create directory for repository: %w", err)
+			// }
+
+			err = git.Clone(git.CloneArgs{
+				URL:  repo.URL,
+				Path: repo.Path,
+				Auth: &repo.Auth,
+			})
+			if err != nil {
+				return fmt.Errorf("failed to clone repository: %w", err)
+			}
+		}
 	}
 }
